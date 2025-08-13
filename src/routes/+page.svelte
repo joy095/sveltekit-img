f9dfd838d6d7c123f47835023efd04e24619b414
Joy <joykarmakar987654321@gmail.com>
2025-08-13 11:25:45 +0530

    project initilized

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<enhanced:img height='400' width='600' style="height: 200px; width: 300px;" src="/static/img.jpeg" alt="some alt text" />
+<img style="height: 200px; width: 300px;" src="/img.jpeg" alt="some alt text" />
